aa04b3b0fc1941baf278c8fedf489a6526e2b97c
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-10-22 13:11:47 -0400

    created form shell

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Form = (props) => {
     return (
-        <h1>Form</h1>
+        <div>
+            <h2>ADD A NEW SONG</h2>
+            <form>
+                <label>TITLE</label>
+                <input type= "text"></input>
+                <label>ARTIST</label>
+                <input type= "text"></input>
+                <label>TIME</label>
+                <input type= "text"></input>
+                <button>ADD NEW SONG</button>
+            </form>
+        </div>
+        
     )
 }
 
