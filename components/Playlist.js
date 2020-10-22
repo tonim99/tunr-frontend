bdff83dd89cb00d505c81929051f79b422bf4707
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-10-22 12:24:32 -0400

    created component templates and wired to App

---
+++ components/Playlist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Playlist = (props) => {
+    return (
+        <h1>Playlist</h1>
+    )
+}
+
+export default Playlist
--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Playlist from '../components/Playlist'
+import Favorites from '../components/Favorites'
+import Form from '../components/Form'
+import Header from '../components/Header'
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Playlist />
+      <Favorites />
+      <Form />
     </div>
   );
 }
